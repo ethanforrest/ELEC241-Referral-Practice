5c493eb4be077d2ee6b2e425768cb25daf74100e
Ethan Forrest <ethan.forrest@students.plymouth.ac.uk>
2021-12-09 02:55:32 +0000

    SPI Changes
    Minor changes such as adding a MOSI = z line and an rx line.

--- SPI Master.vhd
+++ SPI Master.vhd
@@ -7,7 +7,7 @@ use ieee.std_logic_unsigned.all;
 
 entity spi_master is
 
-		generic ( N : natural := 8	);
+		generic ( N : natural := 8);
 
 port(		MISO		: in 	std_logic_vector ((N-1) downto 0);			-- 'Master In Slave Out'
 		Start		: in	std_logic;						-- Starts the system.
@@ -25,7 +25,10 @@ architecture arc of spi_master is
 
 
 signal tx	: std_logic_vector ((N-1) downto 0);						-- Transmit.
-signal output   : std_logic_vector ((N-1) downto 0); 						-- Out.
+signal output   : std_logic_vector ((N-1) downto 0); 						-- Out to slave.
+signal rx	: std_logic_vector ((N-1) downto 0);						-- Recieve.
+signal input	: std_logic_vector ((N-1) downto 0);						-- In from slave.
+
 
 
 
@@ -35,6 +38,8 @@ process (Sys_CLK)
 
 begin
 
+	MOSI <= "ZZZZZZZZ";									-- Always low
+
 	if (start <= '0') then									-- When start is 0, the chip selector is high and the transmit line is held.
 
 	tx <= "ZZZZZZZZ";
@@ -43,6 +48,8 @@ begin
 		elsif (rising_edge(Sys_CLK)) then							-- When Sys_CLK is rising...
 
 		output <= tx; 										-- Set the MOSI to tx.
+		input  <= rx;
+
 		cs <= '0';										-- Chip select is pulled low.
 
 	end if;
@@ -55,10 +62,6 @@ end arc;
 	-- Not entirely confident in what I have written, but think I understand the basis of what is happening in SPI Masters. Need to start, then send the output value to the TX value. 
 	-- I believe this would then be attached to an 'rx' for the slave component.
 	-- This component does compile, which gives me some confidence in it. I am hopeful this component has demonstrated my understanding.
-
-
-	
-
 
 
 
